4d34e7f48831861af3c33f3b6fc7e036ccad2061
sabitetsu <sabitetsu.a24h@gmail.com>
2020-10-10 17:26:45 +0900

    Initial commit

---
+++ Assets/AddRigid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddRigid : MonoBehaviour
+{
+    Rigidbody rb;
+    bool addRigid = false;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (addRigid == false)
+        {
+            if (Input.GetKey(KeyCode.Return))
+            {
+                rb = gameObject.AddComponent<Rigidbody>();
+                addRigid = true;
+            }
+        }
+    }
+}
---
+++ Assets/Tobasu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tobasu : MonoBehaviour
+{
+    public GameObject ball;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void ButtonPushed()
+    {
+        Vector3 pos = ball.transform.position;
+        pos.y += 5;
+        ball.transform.position = pos;
+    }
+}
